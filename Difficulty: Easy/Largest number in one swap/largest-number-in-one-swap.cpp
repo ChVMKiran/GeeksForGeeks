b88bd3b5d1b83a9b5a829145581508ed0bd5d9e7
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-03-09 21:12:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    string largestSwap(string &s) {
+        // code here
+        char maxDigit = '0';
+        int maxi = -1;
+        int l = -1, r = -1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] > maxDigit) {
+                maxDigit = s[i];
+                maxi = i;
+            }
+            else if (s[i] < maxDigit) {
+                l = i;
+                r = maxi;
+            }
+        }
+        if (l == -1) return s;
+        swap(s[l], s[r]);
+        return s;
+    }
+};
