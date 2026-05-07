51d7743d38f7e96612006ce67e89322d90eeab54
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-05-07 16:26:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -2,24 +2,23 @@ class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         // Code here
+        vector<int> dist(V, INT_MAX);
         vector<vector<pair<int, int>>> adj(V);
-        for(auto &edge: edges) {
+        for(auto& edge: edges) {
             adj[edge[0]].push_back({edge[1], edge[2]});
             adj[edge[1]].push_back({edge[0], edge[2]});
         }
-        vector<int> dist(V, INT_MAX);
-        dist[src] = 0;
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, src});
+        dist[src] = 0;
         while(!pq.empty()) {
-            auto top = pq.top();
-            int node = top.second, distance = top.first;
+            auto [cost, node] = pq.top();
             pq.pop();
-            if(distance > dist[node]) continue;
-            for(auto n: adj[node]) {
-                if(distance + n.second < dist[n.first]) {
-                    dist[n.first] = distance + n.second;
-                    pq.push({dist[n.first], n.first});
+            if(cost != dist[node]) continue;
+            for(auto& [nn, c]: adj[node]) {
+                if(dist[nn] > cost + c) {
+                    dist[nn] = cost + c;
+                    pq.push({cost + c, nn});
                 }
             }
         }
